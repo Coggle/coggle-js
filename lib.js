d5f2466e6c0009ec0f1b35791aa83d4c3364470a
James Crosby <james@coggle.it>
2014-05-04 01:35:58 +0100

    make use of jsjsdoc examples support

--- lib.js
+++ lib.js
@@ -5,26 +5,44 @@
  *
  */
 
-/*! imports */
 var unirest   = require('unirest');
 
-/**
- * Create a new Coggle API Node object
+/**!jsjsdoc
+ * // documentation is produced in the order of definition: we want the main
+ * // class to be first, so define it first.
  *
- * @constructor
- * @class CoggleApiNode
+ * doc.CoggleApi = {}
+ * doc.CoggleApiDiagram = {}
+ * doc.CoggleApiNode = {}
  *
- * @param {CoggleApiDiagram} diagram of this node
- * @param {Object} resource representing this node
+ */
+
+/**!jsjsdoc
  *
- * @return constructs an instance of CoggleApi
- * @api public
+ * doc.CoggleApiNode = {
+ *    $brief: "Coggle API Node object, which represents individual parts"+
+ *            "(\"nodes\") of the branches in a Coggle diagarm"
+ * }
+ *
+ * doc.CoggleApiNode.$constructor = {
+ *   $brief: "Create a new instance of a Coggle API Node object.",
+ *   $parameters: {
+ *     coggle_api_diagram: {
+ *      $type:'CoggleApiDiagram',
+ *      $brief:'The Diagram in which this node belongs.'
+ *     },
+ *     node_resource: {
+ *      $type:'Object',
+ *      $brief:'Node Resource object, with at least `_id` (`String`), `text` (`String`), and `offset` (`{x:Number, y:Number}`) fields.'
+ *     }
+ *   }
+ * }
+ *
  */
 var CoggleApiNode = function CoggleApiNode(coggle_api_diagram, node_resource){
   this.diagram = coggle_api_diagram;
   this.id      = node_resource._id;
   this.text    = node_resource.text;
-  this.offset  = node_resource.offset;
   this.offset  = node_resource.offset;
 
   if('parent' in node_resource){
@@ -41,6 +59,26 @@ var CoggleApiNode = function CoggleApiNode(coggle_api_diagram, node_resource){
   }
 };
 CoggleApiNode.prototype = {
+  /**!jsjsdoc
+   *
+   * doc.CoggleApiNode.addChild = {
+   *   $brief: "Add a child to this item. The child is positioned relative to "+
+   *           "this item, and will move when you move this item.",
+   *   $parameters: {
+   *     text: "Text to add for the item.",
+   *     offset: {
+   *        $type: "Object: {x:Number, y:Number}",
+   *        $brief: "Offset of the new item from this one. The `x` coordinate "+
+   *                "is along the branch direction, the `y` coordinate is "+
+   *                "vertically down from the top of the document."
+   *     },
+   *     callback: {
+   *       $type: "Function",
+   *       $brief: "Callback accepting (error, [Array of CoggleApiNode])",
+   *     }
+   *   }
+   * }
+   */
   addChild: function addChild(text, offset, callback){
     var self = this;
     var body = {
@@ -62,20 +100,26 @@ CoggleApiNode.prototype = {
   }
 };
 
-/**
- * Create a new instance of a Coggle API Diagram object.
+/**!jsjsdoc
  *
- * @constructor
- * @class CoggleApi
+ * doc.CoggleApiDiagram = {
+ *    $brief: "Coggle API Diagram object.",
+ * }
  *
- * @param {CoggleApi} api client to be used by this diagram
- * @param {Object} resource describing this diagram. Should have at least the
- *                 following fields:
- *                   * _id: ID of the diagram
- *                   * title: title of the diagram
+ * doc.CoggleApiDiagram.$constructor = {
+ *   $brief: "Create a new instance of a Coggle API Diagram object.",
+ *   $parameters: {
+ *     coggle_api: {
+ *      $type:'CoggleApi',
+ *      $brief:'The API client used for accessing this diagram.'
+ *     },
+ *     diagram_resource: {
+ *      $type:'Object',
+ *      $brief:'Diagram Resource object, with at least `_id` and `title` fields'
+ *     }
+ *   }
+ * }
  *
- * @return constructs an instance of CoggleApi
- * @api public
  */
 var CoggleApiDiagram = function CoggleApiDiagram(coggle_api, diagram_resource){
     this.apiclient = coggle_api;
@@ -89,6 +133,18 @@ CoggleApiDiagram.prototype = {
   replaceId: function replaceId(url){
     return url.replace(':diagram', this.id);
   },
+  /**!jsjsdoc
+   *
+   * doc.CoggleApiDiagram.getNodes = {
+   *   $brief: "Get all of the nodes (branch elements) in a Diagram.",
+   *   $parameters: {
+   *     callback: {
+   *       $type: "Function",
+   *       $brief: "Callback accepting (error, [Array of CoggleApiNode])",
+   *     }
+   *   }
+   * }
+   */
   getNodes: function getNodes(callback){
     var self = this;
     this.apiclient.get(
@@ -114,8 +170,13 @@ CoggleApiDiagram.prototype = {
  *
  * doc.CoggleApi.$constructor = {
  *   $brief: "Create a new instance of the Coggle API client.",
+ *   $examples: "new CoggleApi({token:user_auth_token})",
  *   $parameters: {
- *     options: "Options: {}"
+ *     options: {
+ *      $type: "Object",
+ *      $brief: "Possible Options:\n  * **`token`**, **required**: API user "+
+ *              "authentication token"
+ *     }
  *   }
  * }
  *
@@ -125,98 +186,98 @@ var CoggleApi = function CoggleApi(options){
   this.token   = options.token;
   if(!options.token)
       throw new Error("you must provide a user's authentication token");
-
-  CoggleApi.prototype = {
-    /**!jsjsdoc
-     *
-     * doc.CoggleApi.post = {
-     *   $api: "private",
-     *   $brief: "POST to an endpoint on the Coggle API",
-     *   $parameters: {
-     *     endpoint: {
-     *       $type: "String",
-     *       $brief: "URL of endpoint to post to (relative to the domain)",
-     *       $example: "Use `/api/1/diagrams` to create a new diagram."
-     *     },
-     *     body: "The body to post. Will be converted to JSON.",
-     *     callback: {
-     *       $type: "Function",
-     *       $brief: "Callback accepting (error, body) that will be called "+
-     *               "with the result. The response returned from the server "+
-     *               "is parsed as JSON and returned as `body`.",
-     *     }
-     *   }
-     * }
-     */
-    post: function post(endpoint, body, callback){
-      unirest.post(this.baseurl + endpoint + '?access_token=' + this.token)
-        .type('json')
-        .send(body)
-        .end(function(response){
-          if(!response.ok)
-            return callback(new Error('POST ' + endpoint + ' failed:' + (response.body && response.body.details) || response.error));
-          return callback(false, response.body);
-      });
-    },
-    /**!jsjsdoc
-     *
-     * doc.CoggleApi.get = {
-     *   $api: "private",
-     *   $brief: "GET from an endpoint on the Coggle API",
-     *   $parameters: {
-     *     endpoint: {
-     *       $type: "String",
-     *       $brief: "URL of endpoint to get from (relative to the domain)"
-     *     },
-     *     callback: {
-     *       $type: "Function",
-     *       $brief: "Callback accepting (error, body) that will be called "+
-     *               "with the result. The response returned from the server "+
-     *               "is parsed as JSON and returned as `body`.",
-     *     }
-     *   }
-     * }
-     */
-    get: function get(endpoint, callback){
-      unirest.get(this.baseurl + endpoint + '?access_token=' + this.token)
-        .type('json')
-        .end(function(response){
-          if(!response.ok)
-            return callback(new Error('GET ' + endpoint + ' failed:' + (response.body && response.body.details) || response.error));
-          return callback(false, response.body);
-      });
-    },
-  
-    /**!jsjsdoc
-     *
-     * doc.CoggleApi.createDiagram = {
-     *   $brief: "Create a new Coggle diagram.",
-     *   $parameters: {
-     *     title: {
-     *       $type: "String",
-     *       $brief: "Title for the created diagram."
-     *     },
-     *     callback: {
-     *       $type: "Function",
-     *       $brief: "Callback accepting (error, CoggleApiDiagram)",
-     *     }
-     *   }
-     * }
-     */
-    createDiagram: function createCoggle(title, callback){
-      // callback should accept(error, diagram)
-      var self = this;
-      var body = {
-        title:title
-      };
-      this.post('/api/1/diagrams', body, function(err, body){
-        if(err)
-          return callback(new Error('failed to create coggle: ' + err.message));
-        return callback(false, new CoggleApiDiagram(self, body));
-      });
-    },
-  };
 };
+CoggleApi.prototype = {
+  /**!jsjsdoc
+   *
+   * doc.CoggleApi.post = {
+   *   $api: "private",
+   *   $brief: "POST to an endpoint on the Coggle API",
+   *   $parameters: {
+   *     endpoint: {
+   *       $type: "String",
+   *       $brief: "URL of endpoint to post to (relative to the domain)",
+   *     },
+   *     body: "The body to post. Will be converted to JSON.",
+   *     callback: {
+   *       $type: "Function",
+   *       $brief: "Callback accepting (error, body) that will be called "+
+   *               "with the result. The response returned from the server "+
+   *               "is parsed as JSON and returned as `body`.",
+   *     }
+   *   },
+   *   $examples: ".post(`/api/1/diagrams`, {title:'My New Diagram'}, function(err, diagram){...})"
+   * }
+   */
+  post: function post(endpoint, body, callback){
+    unirest.post(this.baseurl + endpoint + '?access_token=' + this.token)
+      .type('json')
+      .send(body)
+      .end(function(response){
+        if(!response.ok)
+          return callback(new Error('POST ' + endpoint + ' failed:' + (response.body && response.body.details) || response.error));
+        return callback(false, response.body);
+    });
+  },
+  /**!jsjsdoc
+   *
+   * doc.CoggleApi.get = {
+   *   $api: "private",
+   *   $brief: "GET from an endpoint on the Coggle API",
+   *   $parameters: {
+   *     endpoint: {
+   *       $type: "String",
+   *       $brief: "URL of endpoint to get from (relative to the domain)"
+   *     },
+   *     callback: {
+   *       $type: "Function",
+   *       $brief: "Callback accepting (error, body) that will be called "+
+   *               "with the result. The response returned from the server "+
+   *               "is parsed as JSON and returned as `body`.",
+   *     }
+   *   }
+   * }
+   */
+  get: function get(endpoint, callback){
+    unirest.get(this.baseurl + endpoint + '?access_token=' + this.token)
+      .type('json')
+      .end(function(response){
+        if(!response.ok)
+          return callback(new Error('GET ' + endpoint + ' failed:' + (response.body && response.body.details) || response.error));
+        return callback(false, response.body);
+    });
+  },
+
+  /**!jsjsdoc
+   *
+   * doc.CoggleApi.createDiagram = {
+   *   $brief: "Create a new Coggle diagram.",
+   *   $parameters: {
+   *     title: {
+   *       $type: "String",
+   *       $brief: "Title for the created diagram."
+   *     },
+   *     callback: {
+   *       $type: "Function",
+   *       $brief: "Callback accepting (error, CoggleApiDiagram)",
+   *     }
+   *   }
+   * }
+   */
+  createDiagram: function createCoggle(title, callback){
+    // callback should accept(error, diagram)
+    var self = this;
+    var body = {
+      title:title
+    };
+    this.post('/api/1/diagrams', body, function(err, body){
+      if(err)
+        return callback(new Error('failed to create coggle: ' + err.message));
+      return callback(false, new CoggleApiDiagram(self, body));
+    });
+  },
+};
+
 
 module.exports = CoggleApi;
 
--- readme.md
+++ readme.md
@@ -23,15 +23,27 @@ coggle.createDiagram(
 
 
 # API Documentation
+
+
 ##Class `CoggleApi`
 The Coggle API client.
 
 ###Constructor
 Create a new instance of the Coggle API client.
+Example:
+```js
+new CoggleApi({token:user_auth_token})
+```
+Parameters:
+  * **`options`** type: `Object`Possible Options:
+  * **`token`**, **required**: API user authentication token
 
 ###Method `post`
 POST to an endpoint on the Coggle API
-
+Example:
+```js
+.post(`/api/1/diagrams`, {title:'My New Diagram'}, function(err, diagram){...})
+```
 Parameters:
   * **`endpoint`** type: `String`URL of endpoint to post to (relative to the domain)
   * **`body`** The body to post. Will be converted to JSON.
@@ -50,3 +62,38 @@ Create a new Coggle diagram.
 Parameters:
   * **`title`** type: `String`Title for the created diagram.
   * **`callback`** type: `Function`Callback accepting (error, CoggleApiDiagram)
+
+##Class `CoggleApiDiagram`
+Coggle API Diagram object.
+
+###Constructor
+Create a new instance of a Coggle API Diagram object.
+
+Parameters:
+  * **`coggle_api`** type: `CoggleApi`The API client used for accessing this diagram.
+  * **`diagram_resource`** type: `Object`Diagram Resource object, with at least `_id` and `title` fields
+
+###Method `getNodes`
+Get all of the nodes (branch elements) in a Diagram.
+
+Parameters:
+  * **`callback`** type: `Function`Callback accepting (error, [Array of CoggleApiNode])
+
+##Class `CoggleApiNode`
+Coggle API Node object, which represents individual parts("nodes") of the branches in a Coggle diagarm
+
+###Constructor
+Create a new instance of a Coggle API Node object.
+
+Parameters:
+  * **`coggle_api_diagram`** type: `CoggleApiDiagram`The Diagram in which this node belongs.
+  * **`node_resource`** type: `Object`Node Resource object, with at least `_id` (`String`), `text` (`String`), and `offset` (`{x:Number, y:Number}`) fields.
+
+###Method `addChild`
+Add a child to this item. The child is positioned relative to this item, and will move when you move this item.
+
+Parameters:
+  * **`text`** Text to add for the item.
+  * **`offset`** type: `Object: {x:Number, y:Number}`Offset of the new item from this one. The `x` coordinate is along the branch direction, the `y` coordinate is vertically down from the top of the document.
+  * **`callback`** type: `Function`Callback accepting (error, [Array of CoggleApiNode])
+
